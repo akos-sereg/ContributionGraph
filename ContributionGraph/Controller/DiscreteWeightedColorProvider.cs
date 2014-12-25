e59883a7031f32d438739b10fe51cbc5e743a130
akos-sereg <akos.sereg@gmail.com>
2014-12-25 12:38:59 +0100

    Refactoring DiscreteWeightedColorProvider, Demo form

--- ContributionGraph/Controller/DiscreteWeightedColorProvider.cs
+++ ContributionGraph/Controller/DiscreteWeightedColorProvider.cs
@@ -9,28 +9,27 @@ namespace ContributionGraph.Controller
 {
     public class DiscreteWeightedColorProvider : IColorProvider
     {
+        private static readonly Dictionary<long, Color> Thresholds = new Dictionary<long, Color>
+        {
+            { 0, Color.FromArgb(226, 255, 195) },
+            { 5, Color.FromArgb(195, 255, 131) },
+            { 10, Color.FromArgb(156, 255, 52) },
+            { 15, Color.FromArgb(126, 236, 0) },
+        };
+
         public Color GetColor(Model.ContributionItem contributionItem, Model.ContributionList contributionList)
         {
-            if (contributionItem.ContributionCount > 15)
-            {
-                return Color.FromArgb(126, 236, 0);
-            }
-            else if (contributionItem.ContributionCount > 10)
-            {
-                return Color.FromArgb(156, 255, 52);
-            }
-            else if (contributionItem.ContributionCount > 5)
-            {
-                return Color.FromArgb(195, 255, 131);
-            }
-            else if (contributionItem.ContributionCount > 0)
-            {
-                return Color.FromArgb(226, 255, 195);
-            }
-            else
-            {
-                return CalendarView.DEFAULT_COLOR;
-            }
+            var color = CalendarView.DEFAULT_COLOR;
+
+            Thresholds.OrderByDescending(x => x.Key).ToList().ForEach(x => 
+                {
+                    if (x.Key > contributionItem.ContributionCount)
+                    {
+                        color = x.Value;
+                    }
+                });
+
+            return color;
         }
     }
 }
