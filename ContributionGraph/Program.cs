c7b4772161b243f98dcd34db1436664d6daa60c3
akos-sereg <akos.sereg@gmail.com>
2014-12-25 12:41:03 +0100

    Form1 renamed to DemoForm

--- ContributionGraph/Program.cs
+++ ContributionGraph/Program.cs
@@ -16,7 +16,7 @@ namespace ContributionGraph
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new DemoForm());
         }
     }
 }
