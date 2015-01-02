12053f9a796c2adbff83e2bbc8bf0f14d4f691bf
akos.sereg <akos.sereg@gmail.com>
2015-01-02 17:44:08 +0100

    Commit model added

---
+++ ContributionGraph/Model/Commit.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContributionGraph.Model
+{
+    public class Commit
+    {
+        public DateTime Date { get; set; }
+
+        public string Author { get; set; }
+
+        public string Title { get; set; }
+
+        public string AvatarUrl { get; set; }
+
+        public override string ToString()
+        {
+            return this.Title;
+        }
+    }
+}
